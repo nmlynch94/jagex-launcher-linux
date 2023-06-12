81fb010afb8329977a5a664a66d8d6df05c0a721
nmlynch94 <40608755+nmlynch94@users.noreply.github.com>
2023-06-11 22:34:13 -0400

    Update runelite.sh

--- resources/runelite.sh
+++ resources/runelite.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-echo "$SCRIPT_DIR" >> tst.txt
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>"$SCRIPT_DIR/runelite_launch_debug.txt" 2>&1
