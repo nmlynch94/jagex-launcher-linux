24c42632bff572693144adc0d6660829b7f4dc45
nmlynch94 <40608755+nmlynch94@users.noreply.github.com>
2023-06-11 22:34:02 -0400

    Update runelite.sh

--- resources/runelite.sh
+++ resources/runelite.sh
@@ -1,2 +1,8 @@
-#!/bin/sh
-"$(pwd)"/Games/RuneLite/RuneLite.AppImage --appimage-extract-and-run
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+echo "$SCRIPT_DIR" >> tst.txt
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>"$SCRIPT_DIR/runelite_launch_debug.txt" 2>&1
+
+"$SCRIPT_DIR/RuneLite.AppImage" --appimage-extract-and-run
